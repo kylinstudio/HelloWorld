25d2f4397a9b0e533754cbf0cc8b72da8357179b
kylinstudio <kylin0911@gmail.com>
2016-01-27 09:49:37 +0800

    first commit

---
+++ src/cn/kystudio/start/Start.java
@@ -0,0 +1,11 @@
+package cn.kystudio.start;
+/**
+ * 开始类
+ *
+ * Created by ylf on 2016/1/27.
+ */
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+    }
+}
